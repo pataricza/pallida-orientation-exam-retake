b6bc08be9b8fce1f0fe1d55105b2fb1bd537ea79
pataricza <pataricza1987@gmail.com>
2018-04-18 23:42:43 +0200

    practice

--- src/test/java/com/greenfox/demo/controller/WarehouseRestControllerTest.java
+++ src/test/java/com/greenfox/demo/controller/WarehouseRestControllerTest.java
@@ -1,21 +1,29 @@
 package com.greenfox.demo.controller;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.greenfox.demo.service.WarehouseService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.nio.charset.Charset;
 
+import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
@@ -30,8 +38,11 @@ public class WarehouseRestControllerTest {
 
   private MockMvc mockMvc;
 
-  @Autowired
-  WarehouseRestController warehouseRestController;
+  @MockBean
+  WarehouseService warehouseService;
+
+  @MockBean
+  WarehouseRestController restController;
 
   @Autowired
   private WebApplicationContext webApplicationContext;
